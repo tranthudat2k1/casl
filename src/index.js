04331c6eca1307c80b3f013e01db5b71ae5f421d
tranthudat2k1 <thudatdl123@gmail.com>
2022-11-03 18:10:39 +0700

    push code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import { Ability, AbilityBuilder, ForbiddenError } from "@casl/ability";
 
+
+class Posts {
+    constructor(authorId)
+    {
+        this.authorId = authorId;
+        this.content = "ABC",
+        this.view = 0
+    }
+}
+
+const SomePost = new Posts(6)
+
 const user = {
      isAdmin : false,
      name : 'Tran Dat',
-     id : 7
-
+     id : 19
 }
+
 
 // dinh nghia cac kha nang
 const defineAbility = (user) =>{
@@ -16,10 +28,12 @@ const defineAbility = (user) =>{
     can('manage','all') // duoc lam moi thu, all la quan ly duoc moi thu
     }
     else {
-        can('read','Posts');
-        can('crete','Posts');
-        can('update','Posts',['contentt'],{// array nhung truong cho phep update
-            authorId : user.id // neu nhu khong trung id voi author se tra ve false.
+        can('read','Posts',["authorId","view"]);
+        can('create','Posts');
+        can('update','Posts',['content'],{// array nhung truong cho phep update
+            authorId : user.id,
+            authorId : 6
+            // neu nhu khong trung id voi author se tra ve false.
         }); // only if they own it. 
         cannot('delete','Posts').because('Only Admin can delete Posts !!!') // dung Fobident se tra duoc ve loi trong because
     }
@@ -29,23 +43,15 @@ const defineAbility = (user) =>{
     return build();
 }
 
-class Posts {
-    constructor(authorId)
-    {
-        this.authorId = authorId;
-        this.content = "ABC"
-    }
-}
-
-const SomePost = new Posts(7)
-
 // dinh nghia su dung
 const ability = defineAbility(user);
 // check xem co dung khong 
-const isAllowed = ability.can('update',SomePost,'content');
-console.log(isAllowed)
-try {
-    ForbiddenError.from(ability).throwUnlessCan('delete','Posts') // tra ve loi o because
-} catch (error) {
-    console.log(error.message)
-}
+// const isAllowed = ability.can('update',SomePost,'content');
+const isAllowed = ability.can('read',SomePost, "content");
+// const isAllowed1 = ability.can('read',SomePost, "authorId");
+console.log("allow : ",isAllowed)
+// try {
+//     ForbiddenError.from(ability).throwUnlessCan('delete','Posts') // tra ve loi o because
+// } catch (error) {
+//     console.log(error.message)
+// }
